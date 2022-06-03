c21172e3a7b1d1d2d302297376318b946f65244a
Igor Zolov <zolov@i-teco.ru>
2022-06-03 23:07:06 +0300

    Init Commit
    BFS example added;

---
+++ src/main/java/bfs/Tree.java
@@ -0,0 +1,31 @@
+package bfs;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation of Tree objects for BFS algorithms
+ * @param <T>
+ */
+@Data
+public class Tree<T> {
+    private T value;
+    private List<Tree<T>> children;
+
+    public Tree(T value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    public static <T> Tree<T> of(T value) {
+        return new Tree<>(value);
+    }
+
+    public Tree<T> addChild(T value) {
+        Tree<T> newChild = new Tree<>(value);
+        children.add(newChild);
+        return newChild;
+    }
+}
